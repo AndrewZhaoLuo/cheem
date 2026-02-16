fc7011b58d168fffce0c0b4f4296872d56f9aa28
Andrew Luo <andrew.zhao.luo@gmail.com>
2026-02-15 22:09:00 -0800

    debugging in problem

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # batch ids that should go ahead and be scheduled!
-SCALAR_SCHEDULE = [0, 1, 5, 7, 11, 15, 20, 24, 28, 31]
+SCALAR_SCHEDULE = [0, 2, 5, 7, 11, 15, 20, 24, 28, 31]
 
 # "critical": uses critical path heuristic
 # "greedy": simple greedy scheduler
@@ -11,7 +11,7 @@ USE_OPTIMIZED_HASH = True
 
 # Whether to run one round on a simple tree with batch size 8
 # (tree_height, rounds, batch_size)
-WORKLOAD = (10, 16, 256)
+WORKLOAD = (0, 2, 16)
 
 # Whether to enable vselect algorithm for loading
-USE_VSELECT_ALGO = False
+USE_VSELECT_ALGO = True
